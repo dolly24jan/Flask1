728ed9b19a52c12916f064cc82952f8948b40d55
dolly24jan <74894874+dolly24jan@users.noreply.github.com>
2022-01-24 12:57:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,82 +2,19 @@ from flask import Flask,render_template,request,jsonify
 import os
 import codecs
 app= Flask(__name__)
-@app.route('/', defaults={'file_name': "file1.txt",'start_line_no': "0",'end_line_no':"150"})
-@app.route('/<file_name>',defaults={'start_line_no': "0", 'end_line_no':"150"})
-@app.route('/<file_name>/<start_line_no>',defaults={'end_line_no':"150"})
-@app.route('/<file_name>/<start_line_no>/<end_line_no>')
+@app.route('/', defaults={'file_name': "file1.txt",'start_line_no': "0",'end_line_no':"150"}, methods=['GET'])
+@app.route('/<file_name>',defaults={'start_line_no': "0", 'end_line_no':"150"}, methods=['GET'])
+@app.route('/<file_name>/<start_line_no>',defaults={'end_line_no':"150"}, methods=['GET'])
+@app.route('/<file_name>/<start_line_no>/<end_line_no>', methods=['GET'])
 
 def user(file_name,start_line_no,end_line_no):
     try:
-        if file_name =="file1.txt":
+        if (file_name =="file1.txt") or (file_name =="file2.txt") or (file_name =="file3.txt") or (file_name =="file4.txt") :
             if int(start_line_no) < int(end_line_no):
                 s=int(start_line_no)
                 e=int(end_line_no)
                 txt=''
-                with codecs.open(file_name, 'r') as file:
-                    for i, line in enumerate(file):
-                        if(i>=s and i<e+1):
-                            txt= txt+line
-                return render_template("index2.html", content=txt)
-            elif int(start_line_no) > int(end_line_no):
-                s=int(start_line_no)
-                e=int(end_line_no)
-                txt=''
-                return {"status":False,"message":"Not displayed due to invalid input of line no. :"}
-            else:    
-                with codecs.open(file_name, 'rb', encoding='utf8',errors='ignore') as file:
-                    contents = file.read()
-                # return jsonify(contents)
-                return render_template("index2.html", content=contents)
-            
-        if file_name =="file2.txt":
-            if int(start_line_no) < int(end_line_no):
-                s=int(start_line_no)
-                e=int(end_line_no)
-                txt=''
-                with codecs.open(file_name, 'r', encoding='utf8',errors='ignore') as file:
-                    for i, line in enumerate(file):
-                        if(i>=s and i<e+1):
-                            txt= txt+line
-                return render_template("index2.html", content=txt)
-            elif int(start_line_no) > int(end_line_no):
-                s=int(start_line_no)
-                e=int(end_line_no)
-                txt=''
-                return {"status":False,"message":"Not displayed due to invalid input of line no. :"}
-            else:    
-                with codecs.open(file_name, 'rb', encoding='utf8',errors='ignore') as file:
-                    contents = file.read()
-                # return jsonify(contents)
-                return render_template("index2.html", content=contents)
-            
-        elif file_name =="file3.txt":
-            if int(start_line_no) < int(end_line_no):
-                s=int(start_line_no)
-                e=int(end_line_no)
-                txt=''
-                with codecs.open(file_name, 'r') as file:
-                    for i, line in enumerate(file):
-                        if(i>=s and i<e+1):
-                            txt= txt+line
-                return render_template("index2.html", content=txt)
-            elif int(start_line_no) > int(end_line_no):
-                s=int(start_line_no)
-                e=int(end_line_no)
-                txt=''
-                return {"status":False,"message":"Not displayed due to invalid input of line no. :"}
-            else:    
-                with codecs.open(file_name, 'rb', encoding='utf8',errors='ignore') as file:
-                    contents = file.read()
-                # return jsonify(contents)
-                return render_template("index2.html", content=contents)
-
-        elif file_name =="file4.txt":
-            if int(start_line_no) < int(end_line_no):
-                s=int(start_line_no)
-                e=int(end_line_no)
-                txt=''
-                with codecs.open(file_name, 'r', encoding='utf8',errors='ignore') as file:
+                with codecs.open(file_name, 'r',encoding='utf8',errors='ignore') as file:
                     for i, line in enumerate(file):
                         if(i>=s and i<e+1):
                             txt= txt+line
